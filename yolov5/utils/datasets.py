86a273ffb3908e351d26d71276b3de3744d724b0
jinmo <yjmd2222@gmail.com>
2023-08-29 09:44:22 +0900

    rgb2ir paths에서 확장자 .png로 설정

--- yolov5/utils/datasets.py
+++ yolov5/utils/datasets.py
@@ -380,7 +380,7 @@ def img2label_paths(img_paths):
 def rgb2ir_paths(img_paths):
     # Define label paths as a function of image paths
     sa, sb = os.sep + 'images' + os.sep, os.sep + 'ir' + os.sep  # /images/, /labels/ substrings
-    return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.JPG' for x in img_paths]
+    return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.PNG' for x in img_paths]
 
 
 class LoadImagesAndLabels(Dataset):  # for training/testing
